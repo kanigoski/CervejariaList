e42ccab794d110f796f24bc7d659d75acecb8e61
Richard Kanigoski <richardkanigoski1@gmail.com>
2018-11-14 19:53:08 -0200

    Commit inicial

---
+++ Cervejaria/Controllers/CervejaController.cs
@@ -0,0 +1,207 @@
+﻿using Cervejaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cervejaria.Controllers
+{
+    public class CervejaController : Controller
+    {
+
+        // ------ Início --------
+
+        // Cria uma lista de cervejas sem instanciar
+        IList<Cerveja> cervejas;
+
+        // Método construtor
+        public CervejaController()
+        {
+            // Chama o método lerSessao() para pegar
+            // a lista da sessão. Se não houver nada,
+            // Retornará nulo.
+            cervejas = lerSessao();
+            // Caso retorne nulo, execute a rotina abaixo
+            if (cervejas == null)
+            {
+                // Instancia a lista de Cerveja
+                cervejas = new List<Cerveja>();
+
+
+                // Cria alguns objetos do tipo Cerveja
+                Cerveja c1 = new Cerveja(1, "TalerBier", "Quadrupel", 17.17);
+                Cerveja c2 = new Cerveja(2, "Guinness", "Stout", 27.17);
+                Cerveja c3 = new Cerveja(3, "Murphy's", "Stout", 27.17);
+                Cerveja c4 = new Cerveja(4, "New Castle", "Brown Ale", 30.17);
+                Cerveja c5 = new Cerveja(5, "Heineken", "Premium Lager", 4.17);
+
+
+                // Insere na lista
+                cervejas.Add(c1);
+                cervejas.Add(c2);
+                cervejas.Add(c3);
+                cervejas.Add(c4);
+                cervejas.Add(c5);
+
+                // Chama o método salvarSessao passando 
+                // como parâmetro a lista de cervejas criada
+                // e instanciada acima. O método irá salvar
+                // na sessão e, então, poderemos simular
+                salvarSessao(cervejas);
+            }
+
+        }
+
+        // Método que efetua a leitura de uma variável
+        // na sessão - no nosso caso o objeto IList<Cerveja>
+        public IList<Cerveja> lerSessao()
+        {
+            // Cria a lista "nula"
+            IList<Cerveja> temp = null;
+            // Se houver a variável na sessão, traga-a.
+            if (System.Web.HttpContext.Current.Session != null &&
+                System.Web.HttpContext.Current.Session["listaCervejas"] != null)
+            {
+                // Trazendo a variável da sessão e fazendo uma
+                // conversão de tipo (parsing) para a instância
+                // concreta List<Cerveja>
+                temp = (List<Cerveja>)System.Web.HttpContext.Current.Session["listaCervejas"];
+
+            }
+            // Retorna a lista, se houver, ou nulo, se não
+            // houver nada na sessão sobre...
+            return temp;
+        }
+
+        // Armazena a lista na sessão
+        public void salvarSessao(IList<Cerveja> cervejas)
+        {
+            System.Web.HttpContext.Current.Session["listaCervejas"] = cervejas;
+        }
+
+
+        // ------- Fim ----------
+
+        // GET: Cerveja
+        public ActionResult Index()
+        {
+            IList<Cerveja> lista = lerSessao();
+
+            return View("Index" , (IEnumerable<Cerveja>) lista);
+        }
+
+        // GET: Cerveja/Details/5
+        public ActionResult Details(int id)
+        {
+            IList<Cerveja> lista = lerSessao();
+            Cerveja cerveja = lista.Where(objeto => objeto.Id == id).FirstOrDefault();
+
+            return View("Details" , cerveja);
+        }
+
+        // GET: Cerveja/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cerveja/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                IList<Cerveja> lista = lerSessao();
+
+                Cerveja nova = new Cerveja();
+
+                nova.Id = int.Parse(collection["Id"]);
+                nova.Marca = collection["Marca"];
+                nova.Estilo = collection["Estilo"];
+                nova.Preco = double.Parse(collection["Preco"]);
+
+                lista.Add(nova);
+
+                salvarSessao(lista);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Cerveja/Edit/5
+        public ActionResult Edit(int id)
+        {
+            IList<Cerveja> lista = lerSessao();
+            Cerveja cerveja = lista.Where(objeto => objeto.Id == id).FirstOrDefault();
+
+            return View("Edit", cerveja);
+        }
+
+        // POST: Cerveja/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                IList<Cerveja> lista = lerSessao();
+                Cerveja cerveja = lista.Where(objeto => objeto.Id == id).FirstOrDefault();
+
+                lista.Remove(cerveja);
+
+                cerveja.Marca = collection["Marca"];
+                cerveja.Estilo = collection["Estilo"];
+                cerveja.Preco = double.Parse(collection["Preco"]);
+
+                lista.Add(cerveja);
+
+                salvarSessao(lista);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cerveja/Delete/5
+        public ActionResult Delete(int id)
+        {
+            IList<Cerveja> lista = lerSessao();
+            Cerveja cerveja = lista.Where(objeto => objeto.Id == id).FirstOrDefault();
+
+            return View("Delete" , cerveja);
+        }
+
+        // POST: Cerveja/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                IList<Cerveja> lista = lerSessao();
+                Cerveja cerveja = lista.Where(objeto => objeto.Id == id).FirstOrDefault();
+
+                lista.Remove(cerveja);
+
+                salvarSessao(lista);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Cervejaria/Models/Cerveja.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cervejaria.Models
+{
+    public class Cerveja
+    {
+        public Cerveja()
+        {
+
+        }
+
+        public Cerveja (int id, string marca, string estilo, double preco)
+        {
+            this.Id = id;
+            this.Marca = marca;
+            this.Estilo = estilo;
+            this.Preco = preco;
+        }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(20 , MinimumLength = 3 , ErrorMessage = "Deve ter entre 3 e 20 caracteres")]
+        public string Estilo { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [DisplayName("Preço")]
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public double Preco { get; set; }
+
+    }
+}
